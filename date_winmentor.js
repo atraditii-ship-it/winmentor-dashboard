6902a8f16e7077b2a8149410eeda385b8aa6cb6c
atraditii-ship-it <atraditii-ship-it@users.noreply.github.com>
2026-04-03 08:17:09 +0300

    Auto-sync 03.04.2026  8:17:09,47

--- date_winmentor.js
+++ date_winmentor.js
@@ -1,9 +1,9 @@
-﻿// Generat automat din Paradox DB - 2026-04-03 08:14:49
+﻿// Generat automat din Paradox DB - 2026-04-03 08:17:05
 var WINMENTOR_DATA = {
     meta: {
         firma: "IDI25",
         metoda: "Paradox DB (IESIRI+IESIRI1+NART)",
-        data_extragere: "03.04.2026 08:14:49",
+        data_extragere: "03.04.2026 08:17:05",
         an: 2026,
         luna: 3,
         luni_vanzari: 3
